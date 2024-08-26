13bff4c5603603154fe33f0bd848535718569d44
ToshpulatovAlimardon <alimardontoshpulatov001@gmail.com>
2024-08-26 18:10:08 +0500

    changed pretter format

--- assets/js/sendtelegram.js
+++ assets/js/sendtelegram.js
@@ -1,39 +1,49 @@
 //bot token
-let telegram_bot_id = "6432131217:AAHXQlMnOmREGkNwxYqaJ0uVbESAonVLd70"; 
+let telegram_bot_id = "6432131217:AAHXQlMnOmREGkNwxYqaJ0uVbESAonVLd70";
 //chat id
 let chat_id = 1267012660;
 let u_name, email, message, tel, subject;
-let ready = function() {
-    u_name = document.getElementById("name").value;
-    email = document.getElementById("email").value;
-    tel = document.getElementById("tel").value;
-    subject = document.getElementById("subject").value;
-    message = document.getElementById("message").value;
-    message = "Ismi: " + u_name + "\nEmail: " + email + "\nTelefon: " + tel + "\nFan: " + subject + "\nIzoh: " + message;
+let ready = function () {
+  u_name = document.getElementById("name").value;
+  email = document.getElementById("email").value;
+  tel = document.getElementById("tel").value;
+  subject = document.getElementById("subject").value;
+  message = document.getElementById("message").value;
+  message =
+    "Ismi: " +
+    u_name +
+    "\nEmail: " +
+    email +
+    "\nTelefon: " +
+    tel +
+    "\nFan: " +
+    subject +
+    "\nIzoh: " +
+    message;
 };
-let sendtelegram = function() {
-    ready();
-    let settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
-        "method": "POST",
-        "headers": {
-            "Content-Type": "application/json",
-            "cache-control": "no-cache"
-        },
-        "data": JSON.stringify({
-            "chat_id": chat_id,
-            "text": message
-        })
-    };
-    $.ajax(settings).done(function(response) {
-        console.log(response);
-    });
-    document.getElementById("name").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("tel").value = "";
-    document.getElementById("subject").value = "";
-    document.getElementById("message").value = "";
-    return false;
+let sendtelegram = function () {
+  ready();
+  let settings = {
+    async: true,
+    crossDomain: true,
+    url: "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "cache-control": "no-cache",
+    },
+    data: JSON.stringify({
+      chat_id: chat_id,
+      text: message,
+    }),
+  };
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("tel").value = "";
+  document.getElementById("subject").value = "";
+  document.getElementById("message").value = "";
+  return false;
 };
